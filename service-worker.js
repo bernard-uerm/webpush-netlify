cb68daf88d24e4e1c61384d5e48dd0e42fec4244
bernard-uerm <btgresola@uerm.edu.ph>
2020-09-23 08:46:53 +0800

    Initial Commit

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.38e46b27e0423dec96c1edcbb35aafe1.js"
+  "/precache-manifest.60134676d1e0a9479185a773d1c49569.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "etriage-dashboard"});
