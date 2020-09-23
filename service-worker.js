3701a0b3b5e5d8c9f630b9085a042a950b1cdac6
bernard-uerm <btgresola@uerm.edu.ph>
2020-09-23 13:12:13 +0800

    Initial Commit

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.8c4ba25d23f7bee4822d2841df317a04.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.d55378cfdf02693b17d5db17c2c35109.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 // custom service-worker.js
 if (workbox) {
