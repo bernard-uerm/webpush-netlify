71676068918da6f56eacdf4dd676e6cada778c0e
bernard-uerm <btgresola@uerm.edu.ph>
2020-09-23 09:45:12 +0800

    Initial Commit

--- service-worker.js
+++ service-worker.js
@@ -2,9 +2,6 @@ importScripts("/precache-manifest.94b2777a33dfeacebf2dab07634d200c.js", "https:/
 
 // custom service-worker.js
 if (workbox) {
-  // adjust log level for displaying workbox logs
-  workbox.core.setLogLevel(workbox.core.LOG_LEVELS.debug)
-
   // apply precaching. In the built version, the precacheManifest will
   // be imported using importScripts (as is workbox itself) and we can 
   // precache this. This is all we need for precaching
@@ -18,35 +15,6 @@ if (workbox) {
   // Setup cache strategy for Google Fonts. They consist of two parts, a static one
   // coming from fonts.gstatic.com (strategy CacheFirst) and a more ferquently updated on
   // from fonts.googleapis.com. Hence, split in two registerroutes
-  workbox.routing.registerRoute(
-    /^https:\/\/fonts\.googleapis\.com/,
-    new workbox.strategies.StaleWhileRevalidate({
-      cacheName: 'google-fonts-stylesheets',
-    })
-  )
-
-  workbox.routing.registerRoute(
-    /^https:\/\/fonts\.gstatic\.com/,
-    new workbox.strategies.CacheFirst({
-      cacheName: 'google-fonts-webfonts',
-      plugins: [
-        new workbox.cacheableResponse.Plugin({
-          statuses: [0, 200],
-        }),
-        new workbox.expiration.Plugin({
-          maxAgeSeconds: 60 * 60 * 24 * 365,
-          maxEntries: 30,
-        }),
-      ],
-    })
-  )
-
-  workbox.routing.registerRoute(
-    /^https:\/\/stackpath\.bootstrapcdn\.com/,
-    new workbox.strategies.StaleWhileRevalidate({
-      cacheName: 'fontawesome',
-    })
-  );
 }
 
 // This code listens for the user's confirmation to update the app.
@@ -58,6 +26,7 @@ self.addEventListener('message', (e) => {
   switch (e.data) {
     case 'skipWaiting':
       self.skipWaiting();
+      location.reload();
       break;
     default:
       // NOOP
@@ -71,15 +40,16 @@ self.addEventListener('push', (e) => {
   if (e.data) {
     data = e.data.json()
   }
-
+  console.log(e)
+  console.log(data)
   const options = {
     body: data.body,
     icon: '/img/icons/android-chrome-192x192.png',
-    image: '/img/autumn-forest.png',
+    image: '/img/icons/android-chrome-192x192.png',
     vibrate: [300, 200, 300],
-    badge: '/img/icons/plint-badge-96x96.png',
+    badge: '/img/icons/android-chrome-192x192.png',
   }
 
-  e.waitUntil(self.registration.showNotification(data.title, options))
+  e.waitUntil(self.registration.showNotification('UERM ETriage', options))
 })
 
